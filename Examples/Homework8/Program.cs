cde6c09128603ca7b498881b3c2b1c8bd2124f9d
AlexMalygin <maligin86@gmail.com>
2022-02-08 20:52:43 +0300

    четвёртое задача

---
+++ Examples/Homework8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+// подскажите пожалуйста как организовать ввод с целого числа с терминала
+//Console.Write("введите количество чисел N: ");
+//int N = Console.ReadLine();
+int N=10;
+int I = 0;
+while (I < N)
+{
+    int numberA = new Random().Next(1, N);
+    if (numberA%2 == 0)
+    {
+        Console.WriteLine(" четные числа: " + numberA);
+    }
+    I++;
+}
+
