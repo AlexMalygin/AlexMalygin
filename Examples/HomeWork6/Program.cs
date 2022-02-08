61c33a5702b9eb3a429b7916ed4584b0fc25a277
AlexMalygin <maligin86@gmail.com>
2022-02-08 19:55:07 +0300

    решение третьего задания

---
+++ Examples/HomeWork6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Выяснить является ли число чётным
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine("является ли четным числом " + numberA);
+if (numberA%2 == 0)
+{
+Console.WriteLine("четное число: " + numberA);
+}
+else {
+Console.WriteLine("нечетное число: " + numberA);
+}
+
