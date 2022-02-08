be9ade338d543a1796f39ab616445879acd33a0d
AlexMalygin <maligin86@gmail.com>
2022-02-08 19:31:03 +0300

    моё решение первой задачи

---
+++ Examples/HomeWork2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int A = 10;
+int B = 12;
+if (A>B)
+{
+Console.WriteLine("большее число " + A);
+Console.WriteLine("меньшее число " + B);
+}
+else {
+ Console.WriteLine("большее число " + B);
+Console.WriteLine("меньшее число " + A);   
+}
+
