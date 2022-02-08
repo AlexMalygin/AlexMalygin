6372aefba6e201664f38339a658e37423561d4aa
AlexMalygin <maligin86@gmail.com>
2022-02-08 19:44:45 +0300

    решение второй задачи

---
+++ Examples/Homework4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+int max = numberA;
+if (max<numberB) max = numberB;
+if (max<numberC) max = numberC;
+Console.WriteLine("максимальное число: " + max);
