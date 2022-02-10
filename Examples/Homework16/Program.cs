b4335136c5551d6b89d46e701cd1a55e23f041af
AlexMalygin <maligin86@gmail.com>
2022-02-10 11:39:44 +0300

    восьмое задание день недели

---
+++ Examples/Homework16/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 16: Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+
+int GetRandomNumber()
+{
+    int randomNumb = new Random().Next(1,8); // День недели от одного до семи
+    return randomNumb;
+}
+
+bool VihodnoiDen(int number)
+{
+    return number % 6 == 0 || number % 7 == 0;     // Проверяем делится на шестой или седьмой день Без остатка
+}
+int number = GetRandomNumber();
+Console.WriteLine($" День недели: {number}");
+
+if (VihodnoiDen(number)) 
+        Console.WriteLine($"день недели {number} Выходной");
+    else
+        Console.WriteLine($"день недели {number} Рабочий ");
