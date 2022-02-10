10b051a3306e91160c3afe83ab29e95d84da097d
AlexMalygin <maligin86@gmail.com>
2022-02-10 11:21:43 +0300

    седьмое задание решение

---
+++ Examples/Homework14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+
+Console.Write("введите число: ");
+
+string strNumber = Console.ReadLine();
+
+    if(int.TryParse(strNumber, out int intNumber))
+    {
+        string resultValue = intNumber.ToString();
+    if (resultValue.Length >= 3)
+    
+            Console.WriteLine($"третья цифра числа: {resultValue[2]}"); 
+        else
+     Console.WriteLine("число не трёхзначное");
+    }        
+    else 
+
+    Console.WriteLine("Вилли не число");
+    
+
