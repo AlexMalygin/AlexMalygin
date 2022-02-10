562a86487a67acf53c7939b78ba51fe254862a74
AlexMalygin <maligin86@gmail.com>
2022-02-10 10:52:46 +0300

    шестое задание удаление второго числа

---
+++ Examples/Homework12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 12: Удалить вторую цифру трёхзначного числа.
+
+int GetRandomNumber()
+{
+    int randomNumb = new Random().Next(100,1000);
+    return randomNumb;
+}
+int number = GetRandomNumber();
+Console.WriteLine($" Наше трёх значное число : {number}");
+
+int DelSecondDigit(int number)
+{
+string valueStr = number.ToString();
+Console.WriteLine($" Вторая цифра трёх значного числа: {valueStr[1]}"); 
+Console.Write(" Удалим Вторую цифру трёх значного числа: "); 
+Console.WriteLine($"{valueStr[0]}{valueStr[2]}");
+return number;
+}
+
+int result = DelSecondDigit(number);
