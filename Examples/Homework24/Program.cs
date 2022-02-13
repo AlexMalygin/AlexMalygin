4ddc03bc543931f4621f0ef5ef595cbb23ead8f0
AlexMalygin <maligin86@gmail.com>
2022-02-13 16:08:11 +0300

    Задача 24: Найти кубы чисел от 1 до N

---
+++ Examples/Homework24/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 24: Найти кубы чисел от 1 до N
+int Input(string message)   // ВВодим число 
+{
+    Console.Write(message);
+    string numb=Console.ReadLine();
+    return int.Parse(numb);
+}
+void PrintArray(int [] array)  // Печать массива
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+Console.WriteLine();
+}
+int [] FillArray(int number)  // Заполняем массив Кубами чисел
+{
+    int[] array = new int[number];
+    for(int i=0;i<number;i++)
+    {
+        array[i] = (i+1)*(i+1)*(i+1);
+    }
+    return array;
+}
+int n = Input("введите число N больше одного: ");
+Console.WriteLine("Кубы чисел от одного до N");
+var array = FillArray(n);   
+//PrintArray(array);
+
+
+
+
+PrintArray(array);
+
+
+
