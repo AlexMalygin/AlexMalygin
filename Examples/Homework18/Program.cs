2648786971b10720577ac76bd68696ac726533d6
AlexMalygin <maligin86@gmail.com>
2022-02-18 10:05:45 +0300

    Проверить истинность утверждения

---
+++ Examples/Homework18/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить истинность утверждения 
+//¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+void Check(bool x, bool y)
+ {
+     Console.WriteLine($"!({x} || {y}) = {!(x || y)}, !{x} && !{y} = {!x && !y}");
+ }
+
+ Check(false, false);
+ Check(false, true);
+ Check(true, false);
+ Check(true, true); 
