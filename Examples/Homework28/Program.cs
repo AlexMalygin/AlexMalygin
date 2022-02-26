4f9beed01d119056e422edecd7232e2e7dde38a9
AlexMalygin <maligin86@gmail.com>
2022-02-26 15:41:38 +0300

    Подсчитать сумму цифр в числе

---
+++ Examples/Homework28/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 28: Подсчитать сумму цифр в числе
+int Input(string message)   // ВВодим число 
+{
+    Console.Write(message);
+    string numb=Console.ReadLine();
+    return int.Parse(numb);
+}
+int sum = 0;
+int a = Input("введите Целое число A: ");
+int result = a;
+while(result != 0) 
+{
+    sum = sum + result%10; // Вычисляем последнюю цифру числа И складываем её
+    result = result/10;    // Отбрасываем  последнюю цифру числа
+}
+
+Console.WriteLine($"сумма чисел числа A {a} = {sum}");
