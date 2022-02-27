bd0ffce2f5e1268a84ed8223b7ba7761779c913b
AlexMalygin <maligin86@gmail.com>
2022-02-27 14:04:56 +0300

    Определить сколько чисел больше 0 введено с клавиатуры

---
+++ Examples/Homework42/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+int Input(string message)   // ВВодим число
+{
+    Console.Write(message);
+    string numb=Console.ReadLine();
+    return int.Parse(numb);
+}
+
+void VvodBolsheNull(int numb)  
+{
+    int count = 0;
+    for (int i = 0; i < numb; i++)
+     {
+         Console.Write($"Введите число {i + 1}: ");
+         int x = int.Parse(Console.ReadLine());        // Считываем числа С клавиатуры
+         if (x > 0)
+             count++;
+     }
+     Console.WriteLine($"чисел больше нуля: {count}");
+
+ }
+
+ int n = Input("Введите количество чисел: ");
+ VvodBolsheNull(n); 
